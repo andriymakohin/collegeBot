b9aabc8512043794465bc59065e57a65917efacb
Andrii Makohin <andreas2010@ukr.net>
2024-11-10 22:57:13 +0200

    add new bot

--- pm2.config.js
+++ pm2.config.js
@@ -1,17 +1,18 @@
 module.exports = {
-    apps: [{
-        name: 'node-red-bot',
-        script: './node_modules/node-red/red.js',
-        args: '-s settings.js -u ./',
-        watch: false,
-        node_args: '--max_old_space_size=1500',
-        instances: 1,
-        exec_mode: 'cluster',
-        autorestart: true,
-		env: {
-            NODE_ENV: 'dev',
-            PORT: 8080
+  apps: [
+    {
+      name: "node-red-bot",
+      script: "./node_modules/node-red/red.js",
+      args: "-s settings.js -u ./",
+      watch: false,
+      node_args: "--max_old_space_size=1500",
+      instances: 1,
+      exec_mode: "cluster",
+      autorestart: true,
+      env: {
+        NODE_ENV: "dev",
+        PORT: 8080,
+      },
     },
-	}
-    ]
+  ],
 };
